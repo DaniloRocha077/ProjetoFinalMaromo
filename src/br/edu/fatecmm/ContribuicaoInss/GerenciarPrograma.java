501427f039e659b8e2315018f1d28ae3f96f7269
DaniloRocha077 <84972544+DaniloRocha077@users.noreply.github.com>
2022-06-08 11:12:43 -0300

    Add files via upload

---
+++ src/br/edu/fatecmm/ContribuicaoInss/GerenciarPrograma.java
@@ -0,0 +1,10 @@
+package br.edu.fatecmm.ContribuicaoInss;
+
+import br.edu.fatecmm.ContribuicaoInss.view.EmpregadoGUI;
+
+public class GerenciarPrograma {
+
+    public static void main(String[] args) {
+       new EmpregadoGUI();
+    }
+}
---
+++ src/br/edu/fatecmm/ContribuicaoInss/view/GerenciarEmpregadoGUI.java
@@ -0,0 +1,47 @@
+package br.edu.fatecmm.ContribuicaoInss.view;
+
+import br.edu.fatecmm.ContribuicaoInss.models.Empregado;
+import br.edu.fatecmm.ContribuicaoInss.models.Utils;
+import br.edu.fatecmm.ContribuicaoInss.models.GerenciarEmpregado;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+
+public class GerenciarEmpregadoGUI extends JFrame {
+
+    private JPanel page;
+    private JTable table;
+
+    public GerenciarEmpregadoGUI(ArrayList<Empregado> listaEmpregado) {
+        this.setTitle("Gerenciar empregados");
+        this.setLocation((1366/2 - 500/2), (768/2 -450/2));
+        this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+
+        createTable(listaEmpregado);
+        JScrollPane sp = new JScrollPane(table);
+        this.add(sp);
+        this.setSize(700, 400);
+    }
+
+    private void createTable(ArrayList<Empregado> listaEmpregado) {
+        String[] columnNames = {"Código", "Nome", "Setor", "Salário", "Recolhimento"};
+
+        DefaultTableModel tTabela = (DefaultTableModel) table.getModel();
+        tTabela.setNumRows(0);
+        tTabela.setColumnIdentifiers(columnNames);
+        for (int linha = 0; linha < listaEmpregado.size(); linha++) {
+            Empregado empregado = listaEmpregado.get(linha);
+
+            tTabela.addRow(new Object[]{1});
+
+            table.setValueAt(empregado.getCodigoEmpregado(), linha, 0);
+            table.setValueAt(empregado.getNomeEmpregado(), linha, 1);
+            table.setValueAt(empregado.getSetor(), linha, 2);
+            table.setValueAt(Utils.formatarValorDoubleParaString(empregado.getSalarioBruto()), linha, 3);
+            table.setValueAt(Utils.formatarValorDoubleParaString(empregado.getRecInss()), linha, 4);
+        }
+    }
+
+}
